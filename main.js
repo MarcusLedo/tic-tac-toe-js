3a8fe8e773a35a7a4770856a0f9891ec061f4b8a
Marcus Ledo <sabaledo27@hotmail.com>
2025-08-22 09:43:00 -0300

    Create a simple loop to test the multiple methods in the Board object

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 import { Board } from "./Objects/Board.js";
 import promptSync from "prompt-sync";
 
-const board = new Board(8);
 const prompt = promptSync();
+let temp = 0;
 
-//const myName = prompt("Who are you: ");
+const size = prompt("Size of the board: ");
+const board = new Board(size);
 
-//console.log(board.getBoard());
+while(temp < size*size){
+    const coordinates = prompt("Type the coordinates of place you wish to play (ex.: 2 1): ");
+    const x = parseInt(coordinates.slice(0, 1), 10);
+    const y = parseInt(coordinates.slice(2), 10);
 
-board.printBoard();
+    console.log(`"${x}" "${y}"`);
 
-board.updateBoard(7, 5);
+    board.updateBoard(x, y);
+    board.printBoard();
 
-board.printBoard();
+    temp++;
+}
+
+console.log("END");
